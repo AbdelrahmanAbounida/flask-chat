b04a828f61e6ab6a73906c755ebeaf4d0277e922
aboneda <abdelrahmanaboneda@gmail.com>
2024-05-12 07:31:16 +0200

    edit typing

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,8 @@ db = SQLAlchemy()
 bcrypt = Bcrypt()
 
 
-def create_app(config_name: Literal["default","testing","production","development"]="default"):
-    # basedir = os.path.abspath(os.path.dirname(__file__))
+def create_app(config_name: Literal["default","testing","production","development"]="default") -> Flask:
+    """ create a flask app instance """
     app = Flask(__name__,static_url_path="/static")
 
     # App Configuration
@@ -22,19 +22,17 @@ def create_app(config_name: Literal["default","testing","production","developmen
     bcrypt.init_app(app)
 
     # Allow cors
-    # CORS(app)
-    # CORS(app, resources={
-    #     r"/*":{
-    #     "origins":"*"
-    #     }
-    # })
+    CORS(app)
+    CORS(app, resources={
+        r"/*":{
+        "origins":"*"
+        }
+    })
 
     # register blueprints
-    # from routes.chat import chat_router_pb,chat_router_pb_api
     from app.routes.chat import chat_router_pb, message_router_pb
     app.register_blueprint(chat_router_pb)
     app.register_blueprint(message_router_pb)
-
 
     return app
 
